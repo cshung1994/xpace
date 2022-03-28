512bd6817bcb780d2f44d484ccf2fbf742ab7950
andyjjrt <andyjjrt@gmail.com>
2022-03-28 21:11:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="main-page">
+		<LandingFame1Vue />
+	</div>
+</template>
+
+<script>
+import LandingFame1Vue from "./components/LandingFame1.vue"
+
+export default {
+	name: "Home",
+	components: {
+		LandingFame1Vue: LandingFame1Vue
+	}
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './scss/main.scss'
+import '../node_modules/bootstrap/dist/js/bootstrap'
+
+createApp(App).mount('#app')
