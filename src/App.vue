989f9960990290e6cef269b8bb1326e948d2a96d
andyjjrt <andyjjrt@gmail.com>
2022-03-29 00:18:20 +0800

    quick overlook of three pages, they're just skeletons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-	<div class="main-page">
-		<LandingFame1Vue />
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">
+				<img src="./assets/logo.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
+				Xpace
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<div class="d-flex justify-content-end">
+					<ul class="navbar-nav px-3">
+						<li class="nav-item">
+							<router-link class="nav-link" to="/">Home</router-link>
+						</li>
+						<li class="nav-item">
+							<router-link class="nav-link" to="/xcity">XCity</router-link>
+						</li>
+						<li class="nav-item">
+							<router-link class="nav-link" to="/xpet">XPet</router-link>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="email" placeholder="Email" aria-label="email">
+						<button class="btn btn-outline-success" type="submit">Follow</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<br>
+	<div style="background: linear-gradient(180deg, #020915 0%, #340068 100%);">
+		<div class="container py-5 text-white">
+			<router-view></router-view>
+		</div>
 	</div>
+
 </template>
-
-<script>
-import LandingFame1Vue from "./components/LandingFame1.vue"
-
-export default {
-	name: "Home",
-	components: {
-		LandingFame1Vue: LandingFame1Vue
-	}
-}
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="row py-3 gy-3">
+        <div class="col-md-6">
+            <h1>Data Protection  &  Justice Project</h1>
+            <br>
+            <a href="#">Download the Whitepaper</a>
+            <br>
+            <a href="#">View the Onepaper</a>
+        </div>
+        <div class="col-md-6">
+            建立在 web 3.0 技術上<br>
+            以保護用戶資料、資料正義為核心<br>
+            功能強大的次元社群論壇工具<br>
+            一次滿足用戶交友、專取收入的需求
+        </div>
+    </div>
+    <div class="row py-3 gy-3 text-center">
+        <div class="col-md">
+            <div class="card text-black">
+                <h5 class="card-header">BBS X-city</h5>
+                <div class="card-body">
+                    <p class="card-text">
+                        BBS<br>
+                        社群聚會<br>
+                        即時視訊<br>
+                        太空旅行<br>
+                    </p>
+                    <a href="#" class="btn btn-primary">Know More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="card text-black">
+                <h5 class="card-header">X-pet</h5>
+                <div class="card-body">
+                    <p class="card-text">
+                        NFT<br>
+                        交友聊聊<br>
+                        專屬寵物<br>
+                        最適推薦<br>
+                    </p>
+                    <a href="#" class="btn btn-primary">Know More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md">
+            <div class="card text-black">
+                <h5 class="card-header">Surf to Earn</h5>
+                <div class="card-body">
+                    <p class="card-text">
+                        利潤回歸<br>
+                        創造價值<br>
+                        最適配置<br>
+                        DAO<br>
+                    </p>
+                    <a href="#" class="btn btn-primary">Know More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="d-flex">
+        <img src="../assets/logo.png" width="100" height="100">
+        <div>
+            <h1>ECOSYSTEM & APPLICATIONS</h1>
+            <p>第一個專注於資料正義的社群 / 交友 / BBS 論壇工具</p>
+        </div>
+    </div>
+    <br>
+    <div class="px-5">
+        <div>
+            <p>當資料真正的主人：您可以自行決定是否分享 Xpace 生態應用中的個人資料</p>
+            <p>輕鬆獲得利潤回饋：您在 BBS X-city 和 X-pet 所有點擊觀看廣告將可為您賺取收入</p>
+        </div>
+        <div class="card">
+            <div class="card-body text-black">
+                ...
+                <br><br><br><br><br><br>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="d-flex align-items-center">
+        <img src="../assets/logo.png" width="100" height="100">
+        <div>
+            <h1>使命與願景</h1>
+        </div>
+    </div>
+    <br>
+    <div class="px-5 row gy-3">
+        <div class="col-md-7">
+            <p><a href="#">View whitepaper for details</a></p>
+            <p>當全世界還在強調個資保護的落實</p>
+            <p>我們決定再更向前一步邁進全球資料正義的未來</p>
+            <p>讓鉅額的資料利潤不再被大型網路公司所壟斷</p>
+            <p>能夠真正回歸到資料生產者手中</p>
+            <br>
+            <div class="row bg-light rounded">
+                <div class="col-sm-4">
+                    <img src="../assets/feature1.png" class="img-fluid">
+                </div>
+                <div class="col-sm-4">
+                    <img src="../assets/feature2.png" class="img-fluid">
+                </div>
+                <div class="col-sm-4">
+                    <img src="../assets/feature3.png" class="img-fluid">
+                </div>
+                <div class="col-sm-4">
+                    <img src="../assets/feature4.png" class="img-fluid">
+                </div>
+                <div class="col-sm-4">
+                    <img src="../assets/feature5.png" class="img-fluid">
+                </div>
+                <div class="col-sm-4">
+                    <img src="../assets/feature6.png" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <img src="../assets/xpet_dashboard.png" class="img-fluid">
+        </div>
+    </div>
+    
+</template>
