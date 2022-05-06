417a2291875b08c2f5e865f4a0bf0a9a89a6c5ba
An-Ting Hsu <andyhsu10@gmail.com>
2022-05-07 01:27:26 +0800

    feat: update coming soon layout

--- src/views/Xpet.vue
+++ src/views/Xpet.vue
@@ -1,10 +1,11 @@
 <template>
-    <section style="height: 75vh;">
-        <img class="position-absolute img-fluid" src="../assets/xcity/xcity.png">
-        <div class="position-absolute py-4" style="left: 10%;">
+    <section class="position-relative mt-0">
+        <img class="position-absolute w-100 img-fluid" src="../assets/xcity/xcity.png">
+        <div class="position-relative w-100 px-4 py-4">
             <H1>
                 <strong><em>Coming Soon</em></strong>
             </H1>
         </div>
+        <img class="invisible position-relative img-fluid" src="../assets/xcity/xcity.png" style="width: 90%">
     </section>
 </template>
