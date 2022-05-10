5340beefab2a0385ebfc25f4d124808bd5c40f18
An-Ting Hsu <andyhsu10@gmail.com>
2022-05-10 22:33:38 +0800

    feat: follow us url btn

--- src/App.vue
+++ src/App.vue
@@ -64,10 +64,20 @@
             <h1 class="fw-bold mt-4">Join the future community</h1>
             <h1 class="fw-bold">to meet many fun and smart people</h1>
 			<div class="d-flex p-4 justify-content-center">
+				<!--
 				<form class="d-flex">
 					<input class="form-control primary-input me-4" type="email" placeholder="Enter Your E-mail Address" aria-label="email">
 					<button class="btn follow-btn fs-4 fw-bold" type="submit">Follow Us</button>
 				</form>
+				-->
+				<a
+					href=""
+					class="btn follow-btn fs-4 fw-bold"
+					target="_blank"
+					ref="noreferrer"
+				>
+					Follow Us
+				</a>
 			</div>
 			<h1 class="mt-3 fw-light">Get notified about important XPACE updates.</h1>
         </div>
